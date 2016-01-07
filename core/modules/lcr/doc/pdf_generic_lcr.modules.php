9243b8afe3e1c7733ad5b5c67460dcb3b55a6be1
Gauthier <gauthier.verdol@atm-consulting.fr>
2016-01-07 14:51:17 +0100

    fix : qques debugs

--- core/modules/lcr/doc/pdf_generic_lcr.modules.php
+++ core/modules/lcr/doc/pdf_generic_lcr.modules.php
@@ -87,6 +87,8 @@ class pdf_generic_lcr extends ModelePDFFactures {
 		$this->atleastonediscount=0;
 	}
 
+
+
 	function write_file($object,$outputlangs,$srctemplatepath='',$hidedetails=0,$hidedesc=0,$hideref=0, &$TtoGenerate)
 	{
 		global $user,$langs,$conf,$mysoc,$db,$hookmanager;
@@ -165,6 +167,216 @@ class pdf_generic_lcr extends ModelePDFFactures {
 		return 0;   // Erreur par defaut
 	}
 
+	function _pagehead(&$pdf, $object, $showaddress, $outputlangs)
+	{
+		global $conf,$langs;
+
+		$outputlangs->load("main");
+		$outputlangs->load("bills");
+		$outputlangs->load("propal");
+		$outputlangs->load("companies");
+
+		$default_font_size = pdf_getPDFFontSize($outputlangs);
+
+		pdf_pagehead($pdf,$outputlangs,$this->page_hauteur);
+
+		// Show Draft Watermark
+		if($object->statut==0 && (! empty($conf->global->FACTURE_DRAFT_WATERMARK)) )
+        {
+		      pdf_watermark($pdf,$outputlangs,$this->page_hauteur,$this->page_largeur,'mm',$conf->global->FACTURE_DRAFT_WATERMARK);
+        }
+
+		$pdf->SetTextColor(0,0,60);
+		$pdf->SetFont('','B', $default_font_size + 3);
+
+		$posy=$this->marge_haute;
+        $posx=$this->page_largeur-$this->marge_droite-100;
+
+		$pdf->SetXY($this->marge_gauche,$posy);
+
+		// Logo
+		$logo=$conf->mycompany->dir_output.'/logos/'.$this->emetteur->logo;
+		if ($this->emetteur->logo)
+		{
+			if (is_readable($logo))
+			{
+			    $height=pdf_getHeightForLogo($logo);
+				$pdf->Image($logo, $this->marge_gauche, $posy, 0, $height);	// width=0 (auto)
+			}
+			else
+			{
+				$pdf->SetTextColor(200,0,0);
+				$pdf->SetFont('','B',$default_font_size - 2);
+				$pdf->MultiCell(100, 3, $outputlangs->transnoentities("ErrorLogoFileNotFound",$logo), 0, 'L');
+				$pdf->MultiCell(100, 3, $outputlangs->transnoentities("ErrorGoToGlobalSetup"), 0, 'L');
+			}
+		}
+		else
+		{
+			$text=$this->emetteur->name;
+			$pdf->MultiCell(100, 4, $outputlangs->convToOutputCharset($text), 0, 'L');
+		}
+
+		$pdf->SetFont('','B', $default_font_size + 3);
+		$pdf->SetXY($posx,$posy);
+		$pdf->SetTextColor(0,0,60);
+		$title=$outputlangs->transnoentities("Invoice");
+		if ($object->type == 1) $title=$outputlangs->transnoentities("InvoiceReplacement");
+		if ($object->type == 2) $title=$outputlangs->transnoentities("InvoiceAvoir");
+		if ($object->type == 3) $title=$outputlangs->transnoentities("InvoiceDeposit");
+		if ($object->type == 4) $title=$outputlangs->transnoentities("InvoiceProFormat");
+		$pdf->MultiCell(100, 3, $title, '', 'R');
+
+		$pdf->SetFont('','B',$default_font_size);
+
+		$posy+=5;
+		$pdf->SetXY($posx,$posy);
+		$pdf->SetTextColor(0,0,60);
+		$pdf->MultiCell(100, 4, $outputlangs->transnoentities("Ref")." : " . $outputlangs->convToOutputCharset($object->ref), '', 'R');
+
+		$posy+=1;
+		$pdf->SetFont('','', $default_font_size - 2);
+
+		if ($object->ref_client)
+		{
+			$posy+=4;
+			$pdf->SetXY($posx,$posy);
+			$pdf->SetTextColor(0,0,60);
+			$pdf->MultiCell(100, 3, $outputlangs->transnoentities("RefCustomer")." : " . $outputlangs->convToOutputCharset($object->ref_client), '', 'R');
+		}
+
+		if ($object->type == 0 && $objectidnext)
+		{
+			$objectreplacing=new Facture($this->db);
+			$objectreplacing->fetch($objectidnext);
+
+			$posy+=3;
+			$pdf->SetXY($posx,$posy);
+			$pdf->SetTextColor(0,0,60);
+			$pdf->MultiCell(100, 3, $outputlangs->transnoentities("ReplacementByInvoice").' : '.$outputlangs->convToOutputCharset($objectreplacing->ref), '', 'R');
+		}
+		if ($object->type == 1)
+		{
+			$objectreplaced=new Facture($this->db);
+			$objectreplaced->fetch($object->fk_facture_source);
+
+			$posy+=4;
+			$pdf->SetXY($posx,$posy);
+			$pdf->SetTextColor(0,0,60);
+			$pdf->MultiCell(100, 3, $outputlangs->transnoentities("ReplacementInvoice").' : '.$outputlangs->convToOutputCharset($objectreplaced->ref), '', 'R');
+		}
+		if ($object->type == 2)
+		{
+			$objectreplaced=new Facture($this->db);
+			$objectreplaced->fetch($object->fk_facture_source);
+
+			$posy+=3;
+			$pdf->SetXY($posx,$posy);
+			$pdf->SetTextColor(0,0,60);
+			$pdf->MultiCell(100, 3, $outputlangs->transnoentities("CorrectionInvoice").' : '.$outputlangs->convToOutputCharset($objectreplaced->ref), '', 'R');
+		}
+
+		$posy+=4;
+		$pdf->SetXY($posx,$posy);
+		$pdf->SetTextColor(0,0,60);
+		$pdf->MultiCell(100, 3, $outputlangs->transnoentities("DateInvoice")." : " . dol_print_date($object->date,"day",false,$outputlangs), '', 'R');
+
+		if ($object->type != 2)
+		{
+			$posy+=3;
+			$pdf->SetXY($posx,$posy);
+			$pdf->SetTextColor(0,0,60);
+			$pdf->MultiCell(100, 3, $outputlangs->transnoentities("DateEcheance")." : " . dol_print_date($object->date_lim_reglement,"day",false,$outputlangs,true), '', 'R');
+		}
+
+		if ($object->client->code_client)
+		{
+			$posy+=3;
+			$pdf->SetXY($posx,$posy);
+			$pdf->SetTextColor(0,0,60);
+			$pdf->MultiCell(100, 3, $outputlangs->transnoentities("CustomerCode")." : " . $outputlangs->transnoentities($object->client->code_client), '', 'R');
+		}
+
+		$posy+=1;
+
+		if ($showaddress)
+		{
+
+			// Show sender
+			$posy=40;
+			$posx=$this->marge_gauche;
+			if (! empty($conf->global->MAIN_INVERT_SENDER_RECIPIENT)) $posx=$this->page_largeur-$this->marge_droite-80;
+			$hautcadre=38;
+
+			// Show sender frame
+			$pdf->SetTextColor(0,0,0);
+			$pdf->SetFont('','', $default_font_size - 2);
+			$pdf->SetXY($posx,$posy-5);
+			$pdf->MultiCell(66,5, $outputlangs->transnoentities("BillFrom").":", 0, 'L');
+			$pdf->SetXY($posx,$posy);
+			$pdf->SetFillColor(230,230,230);
+			$pdf->MultiCell(92, $hautcadre, "", 0, 'R', 1);
+			$pdf->SetTextColor(0,0,60);
+
+			// Show sender name
+			$pdf->SetXY($posx+2,$posy+3);
+			$pdf->SetFont('','B', $default_font_size);
+			$pdf->MultiCell(90, 4, $outputlangs->convToOutputCharset($this->emetteur->name), 0, 'L');
+			$posy=$pdf->getY();
+
+			// Show sender information
+			$pdf->SetXY($posx+2,$posy);
+			$pdf->SetFont('','', $default_font_size - 1);
+			$pdf->MultiCell(90, 4, $carac_emetteur, 0, 'L');
+
+
+
+			// If BILLING contact defined on invoice, we use it
+			$usecontact=false;
+
+			// Recipient name
+			if (! empty($usecontact))
+			{
+				// On peut utiliser le nom de la societe du contact
+				if (! empty($conf->global->MAIN_USE_COMPANY_NAME_OF_CONTACT)) $socname = $object->contact->socname;
+				else $socname = $object->client->nom;
+				$carac_client_name=$outputlangs->convToOutputCharset($socname);
+			}
+			else
+			{
+				$carac_client_name=$outputlangs->convToOutputCharset($object->client->nom);
+			}
+
+			$carac_client=pdf_build_address($outputlangs,$this->emetteur,$object->client,($usecontact?$object->contact:''),$usecontact,'target');
+
+			// Show recipient
+			$widthrecbox=92;
+			if ($this->page_largeur < 210) $widthrecbox=84;	// To work with US executive format
+			$posy=40;
+			$posx=$this->page_largeur-$this->marge_droite-$widthrecbox;
+			if (! empty($conf->global->MAIN_INVERT_SENDER_RECIPIENT)) $posx=$this->marge_gauche;
+
+			// Show recipient frame
+			$pdf->SetTextColor(0,0,0);
+			$pdf->SetFont('','', $default_font_size - 2);
+			$pdf->SetXY($posx+2,$posy-5);
+			$pdf->MultiCell($widthrecbox, 5, $outputlangs->transnoentities("BillTo").":",0,'L');
+			$pdf->Rect($posx, $posy, $widthrecbox, $hautcadre);
+
+			// Show recipient name
+			$pdf->SetXY($posx+2,$posy+3);
+			$pdf->SetFont('','B', $default_font_size);
+			$pdf->MultiCell($widthrecbox, 4, $carac_client_name, 0, 'L');
+
+			// Show recipient information
+			$pdf->SetFont('','', $default_font_size - 1);
+			$pdf->SetXY($posx+2,$posy+4+(dol_nboflines_bis($carac_client_name,50)*4));
+			$pdf->MultiCell($widthrecbox, 4, $carac_client, 0, 'L');
+		}
+
+		$pdf->SetTextColor(0,0,0);
+	}
+
 	function _showLCR($pdf, $object, $outputlangs, &$TtoGenerate)
 	{
 		global $db, $conf;
@@ -187,19 +399,19 @@ class pdf_generic_lcr extends ModelePDFFactures {
 			$curx=$this->marge_gauche;
 			$cury=$posy-30;			   
 			$pdf->SetFont('','', $default_font_size - 1);
-			$pdf->writeHTMLCell(53, 20, 10, 13, $outputlangs->convToOutputCharset('MERCI DE NOUS RETOURNER LA PRESENTE TRAITE SOUS 8 JOURS.'), 0, 1, false, true, 'J',true);
+			$pdf->writeHTMLCell(53, 20, 10, $cury-8, $outputlangs->convToOutputCharset('MERCI DE NOUS RETOURNER LA PRESENTE TRAITE SOUS 8 JOURS.'), 0, 1, false, true, 'J',true);
 			
 			$pdf->SetFont('','', $default_font_size - 3);
-			$pdf->writeHTMLCell(40, 20, 70, 12, $outputlangs->convToOutputCharset('Contre cette LETTRE DE CHANGE STIPULEE SANS FRAIS'), 0, 1, false, true, 'J',true);
-			$pdf->writeHTMLCell(40, 20, 70, 17.5, $outputlangs->convToOutputCharset('Veuillez payer la somme indiquée ci_dessous à l\'ordre de'), 0, 1, false, true, 'J',true);
+			$pdf->writeHTMLCell(40, 20, 70, $cury-8, $outputlangs->convToOutputCharset('Contre cette LETTRE DE CHANGE STIPULEE SANS FRAIS'), 0, 1, false, true, 'J',true);
+			$pdf->writeHTMLCell(40, 20, 70, $cury-3, $outputlangs->convToOutputCharset('Veuillez payer la somme indiquée ci_dessous à l\'ordre de'), 0, 1, false, true, 'J',true);
 			
 			$pdf->SetFont('','', $default_font_size - 2);
-			$pdf->writeHTMLCell(20, 20, 115, 10, $outputlangs->convToOutputCharset($conf->global->MAIN_INFO_SOCIETE_NOM), 0, 1, false, true, 'J',true);
-			$pdf->writeHTMLCell(40, 20, 115, 14, $outputlangs->convToOutputCharset($conf->global->MAIN_INFO_SOCIETE_ADDRESS), 0, 1, false, true, 'J',true);
-			$pdf->writeHTMLCell(40, 20, 115, 21, $outputlangs->convToOutputCharset($conf->global->MAIN_INFO_SOCIETE_ZIP.' '.$conf->global->MAIN_INFO_SOCIETE_TOWN), 0, 1, false, true, 'J',true);
+			$pdf->writeHTMLCell(20, 20, 115, $cury-8, $outputlangs->convToOutputCharset($conf->global->MAIN_INFO_SOCIETE_NOM), 0, 1, false, true, 'J',true);
+			$pdf->writeHTMLCell(40, 20, 115, $cury-5, $outputlangs->convToOutputCharset($conf->global->MAIN_INFO_SOCIETE_ADDRESS), 0, 1, false, true, 'J',true);
+			$pdf->writeHTMLCell(40, 20, 115, $cury+1, $outputlangs->convToOutputCharset($conf->global->MAIN_INFO_SOCIETE_ZIP.' '.$conf->global->MAIN_INFO_SOCIETE_TOWN), 0, 1, false, true, 'J',true);
 			
 			
-			$pdf->writeHTMLCell(150, 20, 10, 28, $outputlangs->convToOutputCharset('A '.$conf->global->MAIN_INFO_SOCIETE_TOWN.', le'), 0, 1, false, true, 'J',true);
+			$pdf->writeHTMLCell(150, 20, 10, $cury+1, $outputlangs->convToOutputCharset('A '.$conf->global->MAIN_INFO_SOCIETE_TOWN.', le'), 0, 1, false, true, 'J',true);
 			
 
 			//Affichage code monnaie 
@@ -443,7 +655,7 @@ class pdf_generic_lcr extends ModelePDFFactures {
 			$pdf->MultiCell($largeur_cadre, 4, "N° SIREN du tiré",0,R);
 			$pdf->SetXY($curx+$largeur_cadre+2, $cury+16);
 			$pdf->SetFont(pdf_getPDFFont($outputlangs),'B',8);
-			$pdf->MultiCell($largeur_cadre*2.5, 4, $outputlangs->convToOutputCharset($object->client->siren),1,C);
+			$pdf->MultiCell($largeur_cadre*2.5, 4, $outputlangs->convToOutputCharset(empty($object->client->siren) ? $object->client->idprof1 : $object->client->siren),1,C);
 			//signature du tireur
 			$pdf->SetXY($curx+$largeur_cadre*5, $cury);
 			$pdf->SetFont(pdf_getPDFFont($outputlangs),'',6);
@@ -458,15 +670,6 @@ class pdf_generic_lcr extends ModelePDFFactures {
 			
 			$ii++;
 			
-			$res_modulo = $ii%3;
-			if($res_modulo == 1 && $ii >= 4) {
-				$pdf->AddPage('','',true);
-				if (! empty($tplidx)) $pdf->useTemplate($tplidx);
-				$posy =50;
-				$pdf->SetDrawColor(0,0,0);
-			
-				$default_font_size = pdf_getPDFFontSize($outputlangs);
-			}
 			
 		}
 
