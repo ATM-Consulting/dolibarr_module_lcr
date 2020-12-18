30518cb4e1c77cd577fa209631bbe0e4b8f7dd91
boudet jean pascal <68547382+atm-jpb@users.noreply.github.com>
2020-12-18 11:48:14 +0100

    Update modlcr.class.php
    remove  user rights

--- core/modules/modlcr.class.php
+++ core/modules/modlcr.class.php
@@ -183,7 +183,7 @@ class modlcr extends DolibarrModules
 		// $r++;
 		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
 		$this->rights[$r][1] = 'Télécharger les fichiers PDF et CSV des LCR';	// Permission label
-		$this->rights[$r][3] = 1; 					// Permission by default for new user (0/1)
+		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'read';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
 
